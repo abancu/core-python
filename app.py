2b2f28d686da8a3093f99c80ff564260ab616722
Stefan-Adrian Bancu <adrian.bancu@1115it0203.emag.local>
2016-08-03 14:24:53 +0300

    added all locations listening

--- app.py
+++ app.py
@@ -4,4 +4,6 @@ class HelloWorld(object):
     def index(self):
         return "Hello world!"
 if __name__ == '__main__':
+   cherrypy.config.update(
+       {'server.socket_host': '0.0.0.0'} )
    cherrypy.quickstart(HelloWorld())
