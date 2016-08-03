22412c358c75f34c094bb18ad33dff2e39971f14
Stefan-Adrian Bancu <adrian.bancu@1115it0203.emag.local>
2016-08-03 14:31:31 +0300

    correct hipchat characters

--- app.py
+++ app.py
@@ -4,6 +4,5 @@ class HelloWorld(object):
     def index(self):
         return "Hello world!"
 if __name__ == '__main__':
-   cherrypy.config.update(
-       {'server.socket_host': '0.0.0.0'} )
+   cherrypy.config.update({'server.socket_host': '0.0.0.0'})
    cherrypy.quickstart(HelloWorld())
